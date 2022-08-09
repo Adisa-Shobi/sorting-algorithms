f528f75a74b8f0706e529d400b42c19c3613ef84
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-08-09 04:16:08 +0000

    Task 0: bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - Sorts an array of integers using the buble sort algorithm
+ *@array: The array to be sorted
+ *@size: The size of the array.
+ *Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	bool swapped = true;
+	int temp;
+	unsigned int i;
+
+	while (swapped)
+	{
+		swapped = false;
+		for (i = 1; i < size; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				temp = array[i];
+				array[i] = array[i - 1];
+				array[i - 1] = temp;
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+	}
+}
